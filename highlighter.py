aceb802764d452c8115f140eb79b319a28b0a410
kai <karthik.r@endurance.com>
2018-11-30 19:58:18 +0530

    Formatted time

--- highlighter.py
+++ highlighter.py
@@ -7,7 +7,7 @@ def extract(s):
         return []
     ts = matches.group(1) + "-" + matches.group(2) + "-" + matches.group(3)
     return [int(datetime.datetime.strptime(ts, "%d/%m/%y-%I:%M:%S-%p").strftime("%s")), ts , matches.group(4), matches.group(5)]
-with open("chat1.txt", "r") as f:
+with open("./cschat.txt", "r") as f:
     l = f.readlines()
 
 l = [x.strip() for x in l]
@@ -15,15 +15,14 @@ l1 = list(filter(lambda y: len(y) > 0, map(lambda x: extract(x), l)))
 
 
 
-interval = 10
+interval = 30
 start = l1[0][0]
-print(start)
 mg = {}
 for e in l1:
    bucket = int((e[0]-start)/interval) 
    t = mg.get(bucket, [])
    t.append(e)
    mg[bucket] = t
-
-for key,value in sorted(mg.iteritems(), key = lambda (k,v) : len(v)):
-    print(str(key) + "---" + str(interval*key/60) + "--"+str((interval*key)%60) + "--"+ str(len(value)))
+for key,value in sorted(mg.iteritems(), key = lambda (k,v) : len(v), reverse = True)[:25]:
+    
+    print(str(key) + "---" + str(datetime.timedelta(seconds = key*interval)) + "--"+ str(len(value)))
